dbe92290780c54d3d370ce3b99cd9fb67c4f842f
Mikolaj Grobelny <miki4920@gmail.com>
2020-08-23 18:35:01 +0100

    Removed "black" variable by introducing modulus into the if statement

--- ChessBoard.py
+++ ChessBoard.py
@@ -5,9 +5,9 @@ from pyglet import shapes
 class Interface(pyglet.window.Window):
     def __init__(self):
         width, height = self.get_resolution()
-        self.resolution = int(height*0.93)
+        self.resolution = int(height * 0.93)
         super(Interface, self).__init__(width=self.resolution, height=self.resolution)
-        super(Interface, self).set_location(int((width-self.resolution)/2), 0)
+        super(Interface, self).set_location(int((width - self.resolution) / 2), 0)
         self.BOARD_SIZE = 8
         self.chess_board = self.draw_chessboard()
 
@@ -19,24 +19,16 @@ class Interface(pyglet.window.Window):
 
     def draw_chessboard(self):
         square_size = self.resolution / self.BOARD_SIZE
-        black = False
         chess_board = []
         for column in range(0, 8):
-            black = not black
             for row in range(0, 8):
                 chess_board.append(
                     shapes.Rectangle(x=row * square_size, y=column * square_size, width=square_size, height=square_size,
-                                     color=(0, 0, 0) if black else (255, 255, 255)))
-                black = not black
+                                     color=(0, 0, 0) if (column + row) % 2 == 0 else (255, 255, 255)))
         return chess_board
 
     def on_draw(self):
         [square.draw() for square in self.chess_board]
-
-
-
-
-
 
 
 window = Interface()
