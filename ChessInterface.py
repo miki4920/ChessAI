ec00f2dd6827c6bc1dca9231bb2b635d8ff135ac
Mikolaj Grobelny <miki4920@gmail.com>
2020-08-24 20:07:53 +0100

    Added grid snapping and non-collision TODO: Add centralised pieces and tile highlight

--- ChessInterface.py
+++ ChessInterface.py
@@ -16,6 +16,7 @@ class Interface(pyglet.window.Window):
         self.chess_board = ChessBoard().chess_board
         self.draw_chessboard()
         self.current_piece = None
+        self.original_position = None
 
     @staticmethod
     def get_resolution():
@@ -51,23 +52,32 @@ class Interface(pyglet.window.Window):
         [square.draw() for square in self.chess_squares]
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         [piece.draw() for piece in sum(self.chess_board, []) if piece]
-        if self.current_piece and mouse.LEFT:
+        if self.current_piece:
             self.current_piece.draw()
-        elif self.current_piece:
-            position = self.current_piece.position
-            self.chess_board[position[1]][position[0]] = self.current_piece
-            self.current_piece = None
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
         x_modulus = x // self.square_size
         y_modulus = y // self.square_size
         if buttons and mouse.LEFT and not self.current_piece:
             self.current_piece = self.chess_board[y_modulus][x_modulus]
-            if self.current_piece:
-                self.current_piece.position = (x, y)
-                self.chess_board[y_modulus][x_modulus] = None
-        if buttons and mouse.LEFT:
+            self.chess_board[y_modulus][x_modulus] = None
+            self.original_position = x_modulus, y_modulus
+        elif buttons and mouse.LEFT and self.current_piece:
             self.current_piece.position = (x, y)
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        if self.current_piece:
+            x_modulus = x // self.square_size
+            y_modulus = y // self.square_size
+
+            if not self.chess_board[y_modulus][x_modulus]:
+                self.current_piece.position = (x_modulus * self.square_size, y_modulus * self.square_size)
+                self.chess_board[y_modulus][x_modulus] = self.current_piece
+            else:
+                self.chess_board[self.original_position[1]][self.original_position[0]] = self.current_piece
+                self.current_piece.position = self.original_position[0] * self.square_size, self.original_position[
+                    1] * self.square_size
+            self.current_piece = None
 
 
 window = Interface()
