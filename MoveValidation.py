6c8e664e7732ed59b29c1cc09e4d2ea4fed62581
Mikolaj Grobelny <miki4920@gmail.com>
2020-09-04 17:26:03 +0100

    Implemented En Passe TODO: Make En Passe destroy the piece

--- MoveValidation.py
+++ MoveValidation.py
@@ -14,10 +14,14 @@ class ValidateMove(object):
         if piece:
             return self.colour == piece.colour
 
-    def return_change(self):
-        dx = self.move[0] - self.piece.original_position[0]
-        dy = self.move[1] - self.piece.original_position[1]
-        return dx, dy
+    @staticmethod
+    def return_change(move, piece):
+        try:
+            dx = move[0] - piece[0]
+            dy = move[1] - piece[1]
+            return dx, dy
+        except:
+            print(move, piece)
 
     def return_bresenham(self):
         return list(
@@ -31,30 +35,46 @@ class ValidateMove(object):
         return False
 
     def pawn(self):
-        dx, dy = self.return_change()
+        dx, dy = self.return_change(self.move, self.piece.original_position)
         colour = 1 if self.piece.colour else -1
-        if abs(dx) == 1 and colour * dy == 1 and self.tile is not None and self.tile.colour != self.piece.colour:
+        dy = dy * colour
+        if abs(dx) == 1 and dy == 1:
+            if self.tile is not None and self.tile.colour != self.piece.colour:
+                return True
+            if 0 <= self.piece.original_position[0] + colour <= 7:
+                side_tile = self.chess_board.get_tile(
+                    (self.piece.original_position[0] + colour, self.piece.original_position[1]))
+                side_tile = side_tile if side_tile is not None and side_tile.previous_position is not None else None
+                if self.tile is None and side_tile is not None and side_tile.colour != self.piece.colour and abs(
+                        side_tile.original_position[1] - side_tile.previous_position[1]) == 2 and dx == 1:
+                    return True
+            elif 0 <= self.piece.original_position[0] - colour <= 7:
+                side_tile = self.chess_board.get_tile(
+                    (self.piece.original_position[0] - colour, self.piece.original_position[1]))
+                side_tile = side_tile if side_tile is not None and side_tile.previous_position is not None else None
+                if self.tile is None and side_tile is not None and side_tile.colour != self.piece.colour and abs(
+                        side_tile.original_position[1] - side_tile.previous_position[1]) == 2 and dx == -1:
+                    return True
+        elif self.piece.previous_position and dy == 1 and dx == 0 and self.tile is None:
             return True
-        elif self.piece.previous_position and dy * colour == 1 and dx == 0 and self.tile is None:
-            return True
-        elif 1 <= dy * colour <= 2 and dx == 0 and self.validate_path() and not self.piece.previous_position:
+        elif 1 <= dy <= 2 and dx == 0 and self.validate_path() and not self.piece.previous_position:
             return True
         return False
 
     def castle(self):
-        dx, dy = self.return_change()
+        dx, dy = self.return_change(self.move, self.piece.original_position)
         if dx != 0 and dy == 0 or dx == 0 and dy != 0:
             return self.validate_path()
         return False
 
     def knight(self):
-        dx, dy = self.return_change()
+        dx, dy = self.return_change(self.move, self.piece.original_position)
         dx = abs(dx)
         dy = abs(dy)
         return dx == 2 and dy == 1 or dx == 1 and dy == 2
 
     def bishop(self):
-        dx, dy = self.return_change()
+        dx, dy = self.return_change(self.move, self.piece.original_position)
         dx = abs(dx)
         dy = abs(dy)
         if dx == dy and dx != 0:
@@ -65,7 +85,7 @@ class ValidateMove(object):
         return self.bishop() or self.castle()
 
     def king(self):
-        dx, dy = self.return_change()
+        dx, dy = self.return_change(self.move, self.piece.original_position)
         dx = abs(dx)
         dy = abs(dy)
         return dx < 2 and dy < 2 and (dx != 0 or dy != 0)
